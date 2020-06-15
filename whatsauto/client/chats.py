6eaf3832c605719e77f400038a15aa0efedb2611
hexatester <revolusi147id@gmail.com>
2020-06-15 17:41:54 +0700

    refactor resourceId, prepare .from_contact_row_container

--- whatsauto/client/chats.py
+++ whatsauto/client/chats.py
@@ -2,6 +2,15 @@ from .base import BaseClient
 from .conversations import ConversationMethods
 from dataclasses import dataclass
 
+_CHAT_TAB = '//*[@resource-id="com.whatsapp:id/tabs"]/android.widget.LinearLayout[1]/android.widget.FrameLayout[2]'
+_SEARCH_BUTTON = "com.whatsapp:id/menuitem_search"
+_SEARCH_INPUT = "com.whatsapp:id/search_src_text"
+
+_CONTACT_CONTAINER = "com.whatsapp:id/contact_row_container"
+_CONTACT_NAME = "com.whatsapp:id/conversations_row_contact_name"
+
+_ARCHIVED = "com.whatsapp:id/conversations_row_tip_tv"
+
 
 @dataclass
 class Chat:
@@ -10,32 +19,36 @@ class Chat:
     time: str
     counter: int
     group: bool
+    muted: bool
+
+    @staticmethod
+    def from_contact_row_container(widget):
+        return widget
 
 
 class ChatMethods(BaseClient):
+
+    def open_tab_chat(self):
+        self.device.xpath(_CHAT_TAB)
+
     def open_chat(self, contact):
         contact = str(contact)
         self.home()
-        self.device(resourceId="com.whatsapp:id/menuitem_search").click()
-        self.device(resourceId="com.whatsapp:id/search_src_text").click()
+        self.device(resourceId=_SEARCH_BUTTON).click()
+        self.device(resourceId=_SEARCH_INPUT).click()
         self.device.send_keys(contact, clear=True)
-        self.device(
-            resourceId="com.whatsapp:id/conversations_row_contact_name", text=contact).click()
-        return
+        if self.device(
+                resourceId=_CONTACT_NAME, text=contact).exists:
+            self.device(
+                resourceId=_CONTACT_NAME, text=contact).click()
+            return True
+        return False
 
     def open_archived(self):
         if self.home():
             pass
-        if self.device(resourceId="com.whatsapp:id/conversations_row_tip_tv").exists:
+        if self.device(resourceId=_ARCHIVED).exists:
             self.device(
-                resourceId="com.whatsapp:id/conversations_row_tip_tv").click()
-            return True
-        return False
-
-    def send_text(self, contact, message):
-        self.open_chat(contact)
-        if self.device(resourceId="com.whatsapp:id/entry").exists:
-            self.device.send_keys(message, clear=True)
-            self.device.send_action("send")
+                resourceId=_ARCHIVED).click()
             return True
         return False
