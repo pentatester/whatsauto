1f872846952394d41fe46780dc6f3c70d019f6b8
hexatester <revolusi147id@gmail.com>
2020-06-15 17:42:05 +0700

    prepare from_call_row_container

--- whatsauto/client/calls.py
+++ whatsauto/client/calls.py
@@ -20,6 +20,10 @@ class Call(BaseCall):
     contact: str
     retry: int
 
+    @staticmethod
+    def from_call_row_container(widget):
+        return widget
+
 
 class CallMethods(BaseClient):
     def remove(self, call: Call):
