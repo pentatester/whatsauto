4acc409aa97b0d4b93aede569847d54e30e10bd3
hexatester <revolusi147id@gmail.com>
2020-08-23 23:24:06 +0700

    initial dataclasses

--- whatsauto/client/__init__.py
+++
@@ -1,7 +0,0 @@
-from .base import BaseClient
-from .calls import BaseCall, CallInfo, Call, CallMethods
-from .chats import Chat, ChatMethods
-from .contacts import Contact, ContactMethods
-from .conversations import Reply, BaseMessage, StatusMessage, Message, Sticker, ConversationMethods
-
-from .client import Client
