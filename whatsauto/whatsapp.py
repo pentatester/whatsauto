f42205848c301dd2600e4f5df336fd63b717ac2d
hexatester <revolusi147id@gmail.com>
2020-08-29 22:12:27 +0700

    rename to whatsapp

---
+++ whatsauto/whatsapp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""This module contains whatsauto objects"""
+from uiautomator2 import Device
+from queue import Queue
+from whatsauto import Message
+
+
+class WhatsApp:
+    def __init__(self, device: Device = None, max_task: int = 0):
+        if isinstance(device, Device):
+            self.device = device
+        else:
+            self.device = Device(device)
+        self.queue = Queue(maxsize=max_task)
+
+    def message(self, func):
+        pass
+
+    def send_message(self, chat, text: str) -> Message:
+        pass
--- whatsauto/whatsauto.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-"""This module contains whatsauto objects"""
-
-
-class WhatsAuto:
-    pass
