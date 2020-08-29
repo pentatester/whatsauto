134a50c0f35b7fd7c476766cf847d80c6678f1a0
hexatester <revolusi147id@gmail.com>
2020-08-29 22:15:05 +0700

    update props

--- whatsauto/message.py
+++ whatsauto/message.py
@@ -6,15 +6,21 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import Optional
 
-from whatsauto import WhatsAutoObject, User
+from whatsauto import (WhatsAutoObject, User, Group, Document, Location, Photo,
+                       Sticker)
 
 
 @dataclass
 class Message(WhatsAutoObject):
-    user: User
+    user: Optional[User] = None
+    group: Optional[Group] = None
+    document: Optional[Document] = None
+    location: Optional[Location] = None
+    photo: Optional[Photo] = None
+    sticker: Optional[Sticker] = None
     text: Optional[str] = None
     reply_to: Optional[Message] = None
     read: Optional[datetime] = None
     delivered: Optional[datetime] = None
-    pending: Optional[bool] = None
+    pending: Optional[bool] = False
     forwarded: Optional[bool] = False
